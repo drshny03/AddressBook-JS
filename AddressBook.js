9817a58fdd6b48b90ee6931d09f6bcb095a2d8a5
drshny03 <darshanyadav7049@gmail.com>
2025-03-04 15:48:31 +0530

    [Darshan] Add . create a Address Book Contact with first and last names, address, city, state, zip, phone number and email

---
+++ AddressBook.js
@@ -0,0 +1,14 @@
+class Address
+{
+    constructor (fisrtName, lastName, address,city,state,zip,phone,email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
